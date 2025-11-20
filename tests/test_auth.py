9b1ad92dadb900bbd1033eac4ae662fa7902421f
Milosz <m.mokka96@gmail.com>
2025-11-20 14:00:10 +0100

    Added invalid key test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,6 +5,7 @@ import os
 load_dotenv()
 
 def test_auth_valid_key():
+    
     api_key = os.getenv('API_KEY')
     base_url = "https://api.openweathermap.org/data/2.5/weather"
 
@@ -15,11 +16,26 @@ def test_auth_valid_key():
 
     response = requests.get(base_url, params=params)
 
-    # --- Assercje ---
+    data = response.json()\
+        
     assert response.status_code == 200
-    data = response.json()
-
-    # Sprawdzamy czy odpowiedź wygląda jak dane pogodowe
     assert "weather" in data
     assert "main" in data
     assert data["name"] == "London"
+    
+def test_auth_invalid_key():
+    
+    api_key = "111111"
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+    
+    params = {
+        "q": "London",
+        "appid": api_key
+    }
+    
+    response = requests.get(base_url, params=params)
+    
+    data = response.json()
+
+    assert response.status_code == 401
+    assert "Invalid API key" in data["message"]
