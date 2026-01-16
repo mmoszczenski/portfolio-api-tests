96267e06da64a8b6d8720d293ee7154e1771f972
Milosz <m.mokka96@gmail.com>
2026-01-16 17:27:54 +0100

    Improved forecast service method's params

--- services/forecast_service.py
+++ services/forecast_service.py
@@ -1,16 +1,16 @@
 import requests
-
 class ForecastSerivce:
     
     def init(self, client):
         self.client = client
         
-    def get_forecast(self, city=None) -> requests.Response:
+    def get_forecast(self, city=None, api_key=None) -> requests.Response:
         
         params = {}
         
         if city is not None:
-            params["q"]= city
-            
+            params["q"] = city
+        if api_key is not None:
+            params["appid"] = api_key
             
         return self.client.get("forecast", params=params)
