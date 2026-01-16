cbca7017dfcdb66e601373de1e6d4355533d4e08
Milosz <m.mokka96@gmail.com>
2026-01-16 17:23:11 +0100

    Added basic forecast method

--- services/forecast_service.py
+++ services/forecast_service.py
@@ -4,3 +4,13 @@ class ForecastSerivce:
     
     def init(self, client):
         self.client = client
+        
+    def get_forecast(self, city=None) -> requests.Response:
+        
+        params = {}
+        
+        if city is not None:
+            params["q"]= city
+            
+            
+        return self.client.get("forecast", params=params)
