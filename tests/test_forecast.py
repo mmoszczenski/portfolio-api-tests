d60ab8b3ec9aaec62820619d1f8e4abeb64e7623
Milosz <m.mokka96@gmail.com>
2026-01-17 11:30:16 +0100

    Added test for forecast with metric param

--- tests/test_forecast.py
+++ tests/test_forecast.py
@@ -47,10 +47,31 @@ def test_forecast_returns_404_when_city_unkown(forecast, api_key):
     
     response = forecast.get_forecast("askjdfhaksdf", api_key)
     assert response.status_code == 404
+    
     data = response.json()    
     assert "city not found" in data["message"]
     
-def test_forecast_returns_temp_in_C_when_units_metric(forecast, api_key):
+def test_forecast_returns_temperature_in_celsius_when_units_metric(forecast, api_key):
     
-    response = forecast.get_forecast("Warsaw", api_key)
+    city = "Warsaw"
+    
+    response_default = forecast.get_forecast(city, api_key)
+    response_metric = forecast.get_forecast(city, api_key, "metric")
+    
+    assert response_default == 200
+    assert response_metric == 200
+    
+    data_default = response_default.json()
+    data_metric = response_metric.json()
+    
+    default_item = data_default["list"][0]
+    metric_item = data_metric["list"][0]
+    
+    temp_k = default_item["main"]["temp"]
+    temp_c = metric_item["main"]["temp"]
+    
+    difference = abs((temp_k - 273.15) - temp_c)
+    
+    assert difference < 0.2
+    
     
