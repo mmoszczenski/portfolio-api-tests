a67f6afbde5eb435920c89c8c73304275e1f8d09
Milosz_Work <m.mokka96@gmail.com>
2026-01-22 19:29:40 +0100

    Fixed typos in service name

--- conftest.py
+++ conftest.py
@@ -2,36 +2,43 @@ import os
 import pytest
 from services.api_client import ApiClient
 from services.weather_service import WeatherService
-from services.forecast_service import ForecastSerivce
+from services.forecast_service import ForecastService
 from dotenv import load_dotenv
 from utils.cities_loader import load_cities
 from utils.schema_loader import load_schema
 
 load_dotenv()
 
+
 @pytest.fixture
 def api_key():
     return os.getenv("API_KEY")
+
 
 @pytest.fixture
 def client() -> ApiClient:
     return ApiClient()
 
+
 @pytest.fixture
 def weather(client) -> WeatherService:
     return WeatherService(client)
 
+
 @pytest.fixture
-def forecast(client) -> ForecastSerivce:
-    return ForecastSerivce(client)
+def forecast(client) -> ForecastService:
+    return ForecastService(client)
+
 
 @pytest.fixture
 def cities():
     return load_cities()
 
+
 @pytest.fixture
 def weather_schema():
     return load_schema("weather_schema.json")
+
 
 @pytest.fixture
 def forecast_schema():
--- services/forecast_service.py
+++ services/forecast_service.py
@@ -1,18 +1,20 @@
 import requests
-class ForecastSerivce:
-    
+
+
+class ForecastService:
+
     def __init__(self, client):
         self.client = client
-        
+
     def get_forecast(self, city=None, api_key=None, units=None) -> requests.Response:
-        
+
         params = {}
-        
+
         if city is not None:
             params["q"] = city
         if api_key is not None:
             params["appid"] = api_key
         if units is not None:
             params["units"] = units
-            
+
         return self.client.get("/forecast", params=params)
