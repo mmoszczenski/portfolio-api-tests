07dbe6b28deff0d11ae22f5ee0e3271fa5a663f4
Milosz_Work <m.mokka96@gmail.com>
2026-01-24 14:43:48 +0100

    Updated weather service to handle id param

--- services/weather_service.py
+++ services/weather_service.py
@@ -1,14 +1,15 @@
 import requests
 
+
 class WeatherService:
-    
+
     def __init__(self, client):
         self.client = client
-        
-    def get_weather(self, city=None, api_key=None, units=None, lang=None) -> requests.Response:
-        
-        params  = {}
-        
+
+    def get_weather(self, city=None, api_key=None, units=None, lang=None, city_id=None) -> requests.Response:
+
+        params = {}
+
         if city is not None:
             params["q"] = city
         if api_key is not None:
@@ -17,16 +18,16 @@ class WeatherService:
             params["units"] = units
         if lang is not None:
             params["lang"] = lang
-            
-        return self.client.get("/weather", params = params)
-    
+        if city_id is not None:
+            params["id"] = city_id
+
+        return self.client.get("/weather", params=params)
+
     def get_weather_by_coordinates(self, lat, lon, api_key) -> requests.Response:
         params = {
             "lat": lat,
             "lon": lon,
             "appid": api_key
         }
-        
+
         return self.client.get("/weather", params=params)
-        
-    
