70be483cbb92c98ea9a2108fa69552d1ab5caa35
Milosz_Work <m.mokka96@gmail.com>
2026-01-26 18:51:06 +0100

    added additional checks to assertions helper

--- tests/helpers.py/assertions.py
+++ tests/helpers.py/assertions.py
@@ -6,3 +6,17 @@ def assert_response(response, expected_status: int = 200, expected_type: type = 
         f"Expected status {expected_status}, got {response.status_code}"
         f"Response text: {response.text}"
     )
+
+    try:
+        data = response.json()
+    except ValueError as exc:
+        raise AssertionError(
+            f"Response body is not valid JSON. Response text: {response.text}"
+        ) from exc
+
+    assert isinstance(data, expected_type), (
+        f"Expecred response type {expected_type}, got {type(data)}"
+        f"Response data: {data}"
+    )
+
+    return data
