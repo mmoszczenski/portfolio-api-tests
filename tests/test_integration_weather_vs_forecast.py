a6ede1aed47959bf8013f32ef8b66f5820183cee
Milosz <m.mokka96@gmail.com>
2026-01-31 12:58:45 +0100

    adjusted integration test to use assertion helper

--- tests/test_integration_weather_vs_forecast.py
+++ tests/test_integration_weather_vs_forecast.py
@@ -1,8 +1,6 @@
 from constants import DEFAULT_CITY
 from helpers.get_temperature import get_temperature_for_city
 from helpers.assertions import assert_within_tolerance
-from constants import TEMPERATURE_CONVERSION_TOLERANCE
-
 
 def test_weather_and_forecast_return_consistent_temperature(weather, forecast, api_key, assert_status_code_and_valid_json):
 
@@ -18,4 +16,4 @@ def test_weather_and_forecast_return_consistent_temperature(weather, forecast, a
     current_temp = get_temperature_for_city(weather, api_key, city)
     forecast_temp = get_temperature_for_city(forecast, api_key, city)
 
-    assert current_temp - forecast_temp <= tolerance
+    assert_within_tolerance(current_temp, forecast_temp, tolerance)
