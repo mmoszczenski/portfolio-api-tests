1117e42f6099d6ecaf672499a900ef32c1dbd383
Milosz <m.mokka96@gmail.com>
2026-01-31 17:15:21 +0100

    fixed weather service bug

--- services/weather_service.py
+++ services/weather_service.py
@@ -10,9 +10,9 @@ class WeatherService:
     def get_weather(
         self, 
         city: str | None = None,
+        api_key: str | None = None, 
         lat: float | None = None,
         lon: float | None = None,
-        api_key: str | None = None, 
         units: str | None = None, 
         lang: str | None = None, 
         city_id: int | None = None
